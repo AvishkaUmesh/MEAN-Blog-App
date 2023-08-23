ea7ffe3caa130bf69bd750980913aae40a47d740
Avishka Umesh <umesh.avishka@gmail.com>
2023-08-23 16:22:18 +0530

    added login method

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.createUser = (req, res, next) => {
   bcrypt.hash(req.body.password, 10).then((hash) => {
@@ -24,4 +25,34 @@ exports.createUser = (req, res, next) => {
   });
 };
 
-exports.userLogin = (req, res, next) => {};
+exports.userLogin = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user)
+        return res.status(401).json({
+          message: "Auth failed",
+        });
+
+      bcrypt.compare(req.body.password, user.password).then((result) => {
+        if (!result)
+          return res.status(401).json({
+            message: "Auth failed",
+          });
+
+        const token = jwt.sign(
+          { email: user.email, userId: user._id },
+          process.env.JWT_KEY,
+          { expiresIn: "1h" }
+        );
+
+        res.status(200).json({
+          token: token,
+        });
+      });
+    })
+    .catch((err) => {
+      return res.status(401).json({
+        message: "Auth failed",
+      });
+    });
+};
